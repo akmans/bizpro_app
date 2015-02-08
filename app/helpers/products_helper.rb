8098cd715bb08de2c54f77448edb0e36b761351b
Gang Wei <mr.wgang@gmail.com>
2015-02-08 15:35:17 +0000

    Fix percentage

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -72,7 +72,6 @@ module ProductsHelper
 
   # return profit rate
   def profit_rate_help(product, auctions, customs, shipment_details, solds)
-    profit_help(product, auctions, customs, shipment_details, solds) / \
-      product_total_sold_price_help(auctions, customs, shipment_details) * 100
+    profit_help(product, auctions, customs, shipment_details, solds) / product_total_sold_price_help(solds) * 100
   end
 end
