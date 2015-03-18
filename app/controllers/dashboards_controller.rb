2426661005681767d5954c22887d826a1191b1c2
Gang Wei <mr.wgang@gmail.com>
2015-03-18 15:46:10 +0000

    construct dashboard(not finish)

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -8,8 +8,16 @@ class DashboardsController < ApplicationController
   # show action
   def show
     @summary = {}
-    @summary["product"] = product_info
-    
+    # sell info
+    @summary["domestic"] = domestic_sold_info
+    # undeal
+    @summary["undeal_product"] = undeal_product_info
+    @summary["undeal_auction"] = undeal_auction_info
+    @summary["undeal_custom"] = undeal_custom_info
+    # by date
+    @summary["auction"] = auction_info
+    @summary["custom"] = custom_info
+#    debugger
     render 'dashboard'
   end
 
@@ -29,8 +37,70 @@ class DashboardsController < ApplicationController
   # nil
 
   private
-    # get product data info
-    def product_info
+    # get domesitc sold data info
+    def domestic_sold_info
+      domestic = {}
+      domestic["sold_all_cnt"] = Product.where(product_id: PaMap.select("product_id") \
+          .where(auction_id: Auction.where(sold_flg: 1))).all.count
+      domestic["sold_year_cnt"] = Product.where(product_id: PaMap.select("product_id") \
+          .where(auction_id: Auction.where(sold_flg: 1).where("end_time >= :year_s", \
+          {:year_s => Time.zone.now.beginning_of_year}).where("end_time <= :year_e", \
+          {:year_e => Time.zone.now.end_of_year}))).all.count
+      domestic["sold_month_cnt"] = Product.where(product_id: PaMap.select("product_id") \
+          .where(auction_id: Auction.where(sold_flg: 1).where("end_time >= :month_s", \
+          {:month_s => Time.zone.now.beginning_of_month}).where("end_time <= :month_e", \
+          {:month_e => Time.zone.now.end_of_month}))).all.count
+      return domestic
+    end
+
+    # get auction data info
+    def auction_info
+      auction = {}
+      # ------------- sold data --------------------
+      # all auction data(sold)
+      auction["sold_all_cnt"] = count_auction(1, 0)
+      auction["sold_all_amount"] = sum_auction(1, 0)
+      # auction data of current year(sold)
+      auction["sold_year_cnt"] = count_auction(1, 1)
+      auction["sold_year_amount"] = sum_auction(1, 1)
+      # auction date of current month(sold)
+      auction["sold_month_cnt"] = count_auction(1, 2)
+      auction["sold_month_amount"] = sum_auction(1, 2)
+      # ------------- bought data --------------------
+      # all auction data(bought)
+      auction["bought_all_cnt"] = count_auction(0, 0)
+      auction["bought_all_amount"] = sum_auction(0, 0)
+      # auction data of current year(bought)
+      auction["bought_year_cnt"] = count_auction(0, 1)
+      auction["bought_year_amount"] = sum_auction(0, 1)
+      # auction date of current month(bought)
+      auction["bought_month_cnt"] = count_auction(0, 2)
+      auction["bought_month_amount"] = sum_auction(0, 2)
+      return auction
+    end
+
+    # get custom data info
+    def custom_info
+      custom = {}
+      # --------- auction data --------------------
+      # all custom data(is_auction)
+      custom["auction_all"] = count_custom(1, 0)
+      # custom data of current year(is_auction)
+      custom["auction_year"] = count_custom(1, 1)
+      # custom date of current month(is_auction)
+      custom["auction_month"] = count_custom(1, 2)
+      # --------- general data --------------------
+      # all custom data(general)
+      custom["general_all"] = count_custom(0, 0)
+      # custom data of current year(general)
+      custom["general_year"] = count_custom(0, 1)
+      # custom date of current month(general)
+      custom["general_month"] = count_custom(0, 2)
+      return custom
+    end
+
+    # get undeal product data info
+    def undeal_product_info
       product = {}
       # total 
       product["domesitc_cnt"] = Product.where(is_domestic: 1).count
@@ -61,5 +131,51 @@ class DashboardsController < ApplicationController
       finishCustom = Auction.where(auction_id: Custom.select("auction_id, SUM(percentage)")\
           .where("auction_id is not null").group("auction_id").having("SUM(percentage) = 100").pluck(:auction_id)).count
       auction["custom_unregist"] = allCustom - finishCustom
+      return auction
+    end
+
+    # get undeal custom data info
+    def undeal_custom_info
+      custom = {}
+      # product unregist
+      custom["product_unregist"] = Custom.where.not(custom_id: PcMap.all.pluck(:custom_id)).count
+      return custom
+    end
+
+    # ---------------------- utilities function --------------------------
+    # count auction(date_type 0:all 1:year 2:month)
+    def count_auction(sold_flg, date_type)
+      return Auction.where(sold_flg: sold_flg).all.count if date_type == 0
+      beginning_date = Time.zone.now.beginning_of_year
+      beginning_date = Time.zone.now.beginning_of_month if date_type == 2
+      end_date = Time.zone.now.end_of_year
+      end_date = Time.zone.now.end_of_month if date_type == 2
+      return Auction.where(sold_flg: 1).where("end_time >= :year_s", \
+          {:year_s => beginning_date}).where("end_time <= :year_e", \
+          {:year_e => end_date}).all.count
+    end
+
+    # sum auction(date_type 0:all 1:year 2:month)
+    def sum_auction(sold_flg, date_type)
+      return Auction.select("SUM(price) as amount").where(sold_flg: sold_flg).first.amount.to_i if date_type == 0
+      beginning_date = Time.zone.now.beginning_of_year
+      beginning_date = Time.zone.now.beginning_of_month if date_type == 2
+      end_date = Time.zone.now.end_of_year
+      end_date = Time.zone.now.end_of_month if date_type == 2
+      return Auction.select("SUM(price) as amount").where(sold_flg: sold_flg).where("end_time >= :year_s", \
+          {:year_s => beginning_date}).where("end_time <= :year_e", \
+          {:year_e => end_date}).first.amount.to_i
+    end
+
+    # count custom(date_type 0:all 1:year 2:month)
+    def count_custom(is_auction, date_type)
+      return Custom.where(is_auction: is_auction).all.count if date_type == 0
+      beginning_date = Time.zone.now.beginning_of_year
+      beginning_date = Time.zone.now.beginning_of_month if date_type == 2
+      end_date = Time.zone.now.end_of_year
+      end_date = Time.zone.now.end_of_month if date_type == 2
+      return Custom.where(is_auction: is_auction).where("created_at >= :year_s", \
+          {:year_s => beginning_date}).where("created_at <= :year_e", \
+          {:year_e => end_date}).all.count
     end
 end
