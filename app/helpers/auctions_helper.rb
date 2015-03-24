8a3a5997ad9859db37097e863a26a856e0402eee
Gang Wei <ekoo_jp@yahoo.co.jp>
2015-03-24 15:20:16 +0000

    Fix auction select at pa_maps page2

--- app/helpers/auctions_helper.rb
+++ app/helpers/auctions_helper.rb
@@ -124,7 +124,7 @@ module AuctionsHelper
 #    Auction.where(ope_flg: key).where.not(auction_id: PaMap.all).each do |auction|
     Auction.select("auctions.auction_id, auctions.auction_name") \
           .joins("LEFT OUTER JOIN pa_maps ON auctions.auction_id = pa_maps.auction_id") \
-          .where("pa_maps.auction_id is null").each do |auction|
+          .where(ope_flg: 1).where("pa_maps.auction_id is null").each do |auction|
       akey = auction.auction_id
       avalue = auction.auction_name
       new_hash = { akey => avalue}
