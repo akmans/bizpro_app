6e49255057e1b66b371c3f7282096321725ffb6c
Gang Wei <ekoo_jp@yahoo.co.jp>
2015-10-11 14:33:35 +0000

    fix bug

--- app/helpers/cashes_helper.rb
+++ app/helpers/cashes_helper.rb
@@ -2,6 +2,7 @@
 module CashesHelper
   # return yen amount
   def yen_amount_help(cash)
+    return nil if cash.nil?
     return cash.amount if cash.is_domestic == 1
     return cash.amount * 100 / (cash.exchange_rate || 100) unless cash.is_domestic == 1
   end
